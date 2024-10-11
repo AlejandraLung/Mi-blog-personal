27990bf410b3893185c4ec5cefaadf0e8f92cdef
AlejandraLung <alelung594@gmail.com>
2024-10-11 17:36:41 -0500

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log('El formulario fue enviado correctamente');
+  alert('Gracias por tu mensaje. Te contactaremos pronto.');
+});
+
+
+// Efecto de cambio de color en los botones al hacer clic
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    button.style.backgroundColor = '#e74c3c'; // Cambia el color del botón
+    setTimeout(() => {
+      button.style.backgroundColor = '#3498db'; // Vuelve al color original después de 1 segundo
+    }, 1000);
+  });
+});
+
+console.log('El archivo JavaScript está funcionando');  // Esto debe aparecer en la consola del navegador
+
+
+  
